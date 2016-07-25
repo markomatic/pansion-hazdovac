f297aebde9ea7cbca1a746f9257528b374b66662
Marko Matić <mail.marko.matic@gmail.com>
2016-07-25 22:09:39 +0200

    * Page loading

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -4,4 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
+const html = document.getElementsByTagName("html")[0];
+html && html.classList.remove('page-loading');
+
 ReactDOM.render(<App />, document.getElementById('app'));
